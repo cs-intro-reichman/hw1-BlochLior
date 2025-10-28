12797032aaf0907f4cc150a460f27e046c169bb2
Lior Bloch <127208825+BlochLior@users.noreply.github.com>
2025-10-28 19:25:24 +0200

    Implement money distribution among three names
    Added functionality to process multiple names and distribute money equally.

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,15 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+        String name2 = args[1];
+        String name3 = args[2];
+        String money = args[3];
+        int moneyInt = Integer.parseInt(money);
+        double moneyDouble = (double)moneyInt;
+        double value = Math.ceil(moneyDouble/3);
+        String outputString = "Dear " + name1 + ", " + 
+        name2 + ", and " + name3 + ": pay " + 
+        value + " Shekels each.";
+        System.out.println(outputString);
 	}
 }
