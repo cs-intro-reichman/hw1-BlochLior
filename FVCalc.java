2f8c7534cbf288a58af8aaeed0a88b7a9c107757
Lior Bloch <127208825+BlochLior@users.noreply.github.com>
2025-10-28 19:41:24 +0200

    Implement future value calculation in FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,22 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		String currentValueString = args[0];
+        int currentValue = Integer.parseInt(currentValueString);
+        String rateString = args[1];
+        double rate = Double.parseDouble(rateString)/100;
+        String nString = args[2];
+        int n = Integer.parseInt(nString);
+
+        double rateForProduct = Math.pow(rate+1, (double)n);
+        double futureValue = currentValue * rateForProduct;
+
+        String outpuString = "After " + n + " years, $" + 
+            currentValueString + " saved at " + rate*100 + 
+            "% will yield $" + (int)futureValue;
+        
+        System.out.println(outpuString);
+
+
 	}
 }
