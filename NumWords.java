c1d8f11ef57f88020910f20619a8cca6cd692275
Lior Bloch <127208825+BlochLior@users.noreply.github.com>
2025-10-28 19:49:16 +0200

    Implement number printing in words
    Extracts hundreds, tens, and ones from the input number and formats the output string accordingly.

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    String numString = args[0];
+        int num = Integer.parseInt(numString);
+        int hundreds = num/100%10;
+        int tens = num/10%10;
+        int ones = num%10;
+        String outString = "" + hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.";
+        System.out.println(outString);
 	}
 }
